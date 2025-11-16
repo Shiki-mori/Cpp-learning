fcd5614820c7939c6b6f6f21b6af7b3055971cda
Shiki-mori <2296690003@qq.com>
2025-11-16 17:07:41 +0800

    start chapter7-function, finished preface and 7.1.1

---
+++ src/cppChapter7.cpp
@@ -0,0 +1,35 @@
+﻿// //7.1 calling.cpp --defining, prototyping, and calling a function
+// #include<iostream>
+// void simple(); //function prototype
+// int main(){
+//     using namespace std;
+//     cout<<"main() will call the simple() function: \n";
+//     simple();  //function call
+//     cout<<"main() is finished with the simple() function.\n";
+//     //cin.get();
+//     return 0;
+// }
+
+// //function definition
+// void simple(){
+//     using namespace std;
+//     cout<<"I'm but a simple function.\n";
+// }
+
+// // 7.1.1 cube.cpp -- testing cube() function
+// #include<iostream>
+// using namespace std;
+// double cube(double x); //function prototype
+// int main(){
+//     double a = 1.2;
+//     cout<<"The cube of "<<a<<" is "<<cube(a)<<".\n";
+//     a = 5.0;
+//     cout<<"The cube of "<<a<<" is "<<cube(a)<<".\n";
+//     return 0;
+// }
+// double cube(double x){ //function definition
+//     return x*x*x;
+// }
+
+
+
