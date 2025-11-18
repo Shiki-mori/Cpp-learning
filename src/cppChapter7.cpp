ba869462b0ce93b0bc73951675266915c7cc5d54
Shiki-mori <2296690003@qq.com>
2025-11-19 00:58:09 +0800

    finished 7.1

--- src/cppChapter7.cpp
+++ src/cppChapter7.cpp
@@ -31,5 +31,29 @@
 //     return x*x*x;
 // }
 
+// // 7.2 protos.cpp -- using prototypes and function calls
+// #include <iostream>
+// void cheers(int);
+// double cube(double x);
+// int main() {
+//   using namespace std;
+//   cheers(5); // function call
+//   cout << "Give me a number: ";
+//   double side;
+//   cin >> side;
+//   double volume = cube(side); // function call
+//   cout << "A " << side << "-foot cube has a volumn of ";
+//   cout << volume << " cubic feet.\n";
+//   cheers(cube(2));
+//   return 0;
+// }
 
+// void cheers(int n) {
+//   using namespace std;
+//   for (int i = 0; i < n; i++) {
+//     cout << "Cheers! ";
+//   }
+//   cout << endl;
+// }
 
+// double cube(double x) { return x * x * x; }
