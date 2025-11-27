d15c25943fe4cd7bd166f7da1f18e36de0bda9c4
Shiki-mori <2296690003@qq.com>
2025-11-28 01:45:18 +0800

    completed 7.2.1

--- src/cppChapter7.cpp
+++ src/cppChapter7.cpp
@@ -57,3 +57,30 @@
 // }
 
 // double cube(double x) { return x * x * x; }
+
+// // 7.3 twoarg.cpp --a function with two arguments
+// #include <iostream>
+// using namespace std;
+// void n_chars(char, int); // function prototype
+// int main() {
+//   int times;
+//   char ch;
+
+//   cout << "Enter a character: ";
+//   cin >> ch;
+//   while (ch != 'q') { // q to quit
+//     cout << "Enter an integer: ";
+//     cin >> times;
+//     n_chars(ch, times); // function with two arguments
+//     cout << "\nEnter another character or press the q-key to quit: ";
+//     cin >> ch;
+//   }
+//   cout << "The value of times is " << times << ".\n";
+//   cout << "Bye.\n";
+//   return 0;
+// }
+
+// void n_chars(char c, int n) { // displays c n times
+//   while (n-- > 0)             // continue until n reaches 0
+//     cout << c;
+// }
